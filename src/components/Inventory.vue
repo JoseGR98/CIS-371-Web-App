304a32c29436ee26c263a9341fc98865782045ef
Sheehann <44250997+Sheehann@users.noreply.github.com>
2019-12-04 01:28:44 -0500

    Add files via upload

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,14 +1,20 @@
 
 <template>
   <v-container>
-      <h1> THIS IS inventory </h1>
-        <div>
-            
+      <h1> Store Inventory </h1>
+        <div class="Grid">
+            <th>
+                <tr>
+                    <td>Item Name</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                </tr>
+            </th>
             <tr v-for="(products, pos) in products" :key="pos">
             <td>{{products.product}}</td>
             <td>{{products.quantity}}</td>
             <td>{{products.price}}</td>
-            <v-btn @click="Sale" color="primary">Buy Now!</v-btn>
+            <v-btn @click="sales" color="primary">Buy Now!</v-btn>
             </tr>
             
         </div> 
@@ -17,6 +23,7 @@
 
 <script>
 import { AppDB } from "../db-init.js";
+import { AppAUTH } from "../db-init.js";
 export default {
   name: 'Inventory',
 
@@ -25,18 +32,50 @@ export default {
     } ),
     mounted() {
         AppDB.ref("inventory").on("child_added", this.additem);
+          AppAUTH.onAuthStateChanged((user) => {
+      if (user) {
+        this.userID = user.uid;
+      } else {
+          // User not logged in or has just logged out.
+         }
+});
     },
   methods: {
       additem(snapshot){
         const item = snapshot.val();
         this.products.push({...item, mykey:snapshot.key})
+      },
+    sales(){
+        AppDB.ref("inventory").push().set({
+        product: this.productName,
+        quantity: this.quantity,
+        price: this.price
+  });
+    AppDB.ref("orderHistory/" + this.userID)
+    .push()
+    .set({
+        product: this.productName,
+        quantity: this.quantity,
+        price: this.price,
+        typeOfTransaction: "Buying"
+  });
       }
-      //sale(){}
   }
 }
 </script>
 
 <style>
+.Grid {background-color: #fff; margin: 5px 0 10px 0; border: solid 1px #525252; border-collapse:collapse; font-family:Calibri; color: #474747;}
 
+.Grid td {
+
+      padding: 20px;
+
+      border: solid 1px black; }
+
+.Grid th  {
+
+      padding : 40px 20px;
+      font-size: 0.9em; }
 
 </style>
--- src/components/OrderHistory.vue
+++ src/components/OrderHistory.vue
@@ -1,6 +1,24 @@
 <template>
   <v-container>
-      <h1> THIS IS ORDER </h1>
+      <h1> Order History </h1>
+        <div class="Grid">
+          <table>
+            <th>
+                <tr>
+                    <td>Item Name</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                    <td>Transaction</td>
+                </tr>
+            </th>
+            <tr v-for="(products, pos) in products" :key="pos">
+            <td>{{products.price}}</td>
+            <td>{{products.product}}</td>
+            <td>{{products.quantity}}</td>
+            <td>{{products.typeOfTransaction}}</td>
+            </tr>
+            </table>
+        </div> 
   </v-container>
 </template>
 
@@ -8,35 +26,39 @@
 import { AppDB } from "../db-init.js";
 import { AppAUTH } from "../db-init.js";
 
-export default {
-    
+export default {  
   name: 'OrderHistory',
-
   data: () => ( {
-      product: "",
-      price: "",
-      quantity: "",
-      typeOfTransaction: ""
+      products: []
   } ),
-  mounted() {
-    AppAUTH.onAuthStateChanged((user) => {
-  if (user){
-    this.userID = user.uid
-  }else{
-    //user is not logged in
-  }
-  });
-  },
+    mounted() {
+        AppDB.ref("orderHistory").on("child_added", this.additem);
+          AppAUTH.onAuthStateChanged((user) => {
+      if (user) {
+        this.userID = user.uid;
+      } else {
+          // User not logged in or has just logged out.
+         }
+});
+    },
   methods: {
-    addData(){
-     AppDB.ref("orderHistory/" + this.userID).push().set({
-      product: this.productname,
-      price: this.price,
-      quantity: this.quantity,
-      typeOfTransaction: "Selling"
-     });     
-    }
+    additem(snapshot){
+        const items = snapshot.val();
+        this.products.push({...items, mykey:snapshot.key})
+      },
   }
 }
-
 </script>
+
+<style>
+.Grid { margin: 5px 0 10px 0; border: solid 1px #525252;}
+
+ td {
+      padding: 2px;
+      border: solid 1px black; }
+
+ th  {
+      padding : 40px 20px;
+      font-size: 0.9em; }
+
+</style>
